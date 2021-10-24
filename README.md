e9ec519c079cfa3f8207a99a0d19af1fe3bfc4e2
Matias <matias.lappalainen@loikka.io>
2021-10-24 22:27:57 +0300

    Add client and server skeletons

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # koodikerho-full-stack
+
+Täältä löydät pohjat sekä frontendille, että backendille.
+
+Frontend löytyy kansiosta client jonka voi käynnistää ajamalla komennon: `npm start`. Frontend käyttää ReactJs teknologiaa josta voi lukea lisää täältä [ReactJs frontpage](https://reactjs.org/)
+
+Backend löytyy kansiosta server ja sen voi käynnistää komennolla `npm start`
--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
-import BlogCard from './components/blog-card';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BlogCard headerImg="https://loremflickr.com/1080/400" title="Hello world" body="
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac tortor non urna aliquet gravida. Sed dignissim, elit sed lacinia ullamcorper, leo diam aliquet erat, et sagittis odio odio ac enim. Mauris ipsum nisl, consequat quis felis at, lobortis semper sapien. Aliquam a faucibus mi. Sed vehicula nunc quis tellus aliquam pulvinar. Sed ultricies, nunc a luctus pretium, orci sapien fermentum justo, nec commodo mi purus in lectus. Cras venenatis aliquet cursus. Morbi vitae metus et ligula venenatis faucibus. Etiam vel ex vitae ex mollis sodales quis sit amet neque. Cras semper vitae nibh sed tincidunt. Nam finibus mattis nibh, nec semper sem semper nec.
-
-Nulla mattis tellus ut dignissim tristique. Curabitur et sapien gravida, eleifend diam et, dignissim urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at consectetur sem, in pretium erat. Aenean nisi sem, dictum ac lacinia ornare, elementum quis sem. Maecenas ut finibus purus. Duis tincidunt ut diam at congue. Donec faucibus sapien enim, in scelerisque quam vestibulum a. Praesent tempus eget neque eu scelerisque. Cras felis justo, bibendum a mi in, sodales fermentum massa.
-
-Pellentesque venenatis laoreet enim, eu accumsan felis condimentum eget. Pellentesque non suscipit metus. Aenean hendrerit lorem ac massa pellentesque ornare. Sed rutrum est non massa egestas, eget vulputate mi mollis. Phasellus quis porta diam. Nullam nec pellentesque metus, at bibendum leo. Nullam consectetur molestie tellus, viverra tincidunt enim luctus quis. Sed commodo lacus leo, nec varius arcu blandit non.
-
-Nulla lorem turpis, dictum ornare elit vitae, accumsan iaculis ipsum. Vivamus id ultricies tortor. Morbi vitae ex ipsum. Sed pretium, orci non interdum maximus, nunc purus gravida nisl, vitae faucibus arcu sem nec sem. Sed vulputate non ligula vitae luctus. Quisque at posuere velit, non interdum enim. Vestibulum fringilla lobortis rhoncus.
-
-Quisque tempus odio sed ligula sodales, ut tempor magna varius. Sed venenatis dignissim felis, at viverra lorem pretium quis. Etiam imperdiet ac risus nec consequat. Pellentesque pulvinar volutpat lacus, nec hendrerit sem iaculis eu. Aliquam lacinia consequat turpis, et consectetur enim blandit eu. Nam eget interdum neque. Duis elementum venenatis turpis, vel convallis turpis aliquet vitae. Nam efficitur, justo sit amet ornare mollis, est orci imperdiet ante, non sollicitudin augue mi vitae dolor. Proin at ex tincidunt, luctus lectus vel, tristique quam. Aenean lacinia diam quis justo imperdiet, ut cursus velit ultrices."/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
