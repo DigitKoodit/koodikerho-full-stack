c1c9f43bf723a947aea239e0cbea26b7f16264d4
Petro Silenius <petro.silenius@gmail.com>
2021-10-25 17:25:53 +0300

    Fix form submit

--- client/src/components/create-blog-modal.jsx
+++ client/src/components/create-blog-modal.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const CreateBlogModal = ({ createPost, closeModal }) => {
 
   const [state, setState] = useState({
-    url: "",
+    headerImg: "",
     title: "",
     body: "",
   });
@@ -14,13 +14,13 @@ const CreateBlogModal = ({ createPost, closeModal }) => {
       <div className="create-blog-wrapper">
         <div className="close-button-wrapper"><button className="close-button" onClick={closeModal}><i>x</i></button></div>
         <form>
-          <label htmlFor="url">Kuvan url</label>
-          <input type="text" name="url" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.url} />
+          <label htmlFor="headerImg">Kuvan url</label>
+          <input type="text" name="headerImg" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.headerImg} />
           <label htmlFor="title">Otsikko</label>
           <input type="text" name="title" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.title} />
           <label htmlFor="body">Teksti</label>
           <textarea name="body" id="body" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.body} />
-          <button className="submit-button" onClick={() => createPost({ ...state, headerImg: state.url })}>Luo blogi</button>
+          <button className="submit-button" onClick={(e) => {e.preventDefault();createPost({ ...state });closeModal();}}>Luo blogi</button>
         </form>
       </div>
     </div>, document.body
