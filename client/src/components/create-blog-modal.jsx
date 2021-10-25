8bb6c0dead1c9bcbeafa9406507fe35459efb006
Petro Silenius <petro.silenius@gmail.com>
2021-10-25 17:38:43 +0300

    Fix code indents, refactor submit form function

--- client/src/components/create-blog-modal.jsx
+++ client/src/components/create-blog-modal.jsx
@@ -9,18 +9,41 @@ const CreateBlogModal = ({ createPost, closeModal }) => {
     body: "",
   });
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    createPost(state);
+    closeModal();
+  }
+
   return process.browser && ReactDOM.createPortal(
     <div className="create-blog-modal">
       <div className="create-blog-wrapper">
-        <div className="close-button-wrapper"><button className="close-button" onClick={closeModal}><i>x</i></button></div>
+        <div className="close-button-wrapper">
+          <button className="close-button" onClick={closeModal}><i>x</i></button>
+        </div>
         <form>
           <label htmlFor="headerImg">Kuvan url</label>
-          <input type="text" name="headerImg" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.headerImg} />
+          <input
+            type="text"
+            name="headerImg"
+            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+            value={state.headerImg}
+          />
           <label htmlFor="title">Otsikko</label>
-          <input type="text" name="title" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.title} />
+          <input
+            type="text"
+            name="title"
+            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+            value={state.title}
+          />
           <label htmlFor="body">Teksti</label>
-          <textarea name="body" id="body" onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })} value={state.body} />
-          <button className="submit-button" onClick={(e) => {e.preventDefault();createPost({ ...state });closeModal();}}>Luo blogi</button>
+          <textarea
+            name="body"
+            id="body"
+            onChange={(e) => setState({ ...state, [e.target.name]: e.target.value })}
+            value={state.body}
+          />
+          <button className="submit-button" onClick={submitHandler}>Luo blogi</button>
         </form>
       </div>
     </div>, document.body
