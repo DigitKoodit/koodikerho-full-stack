b1cb3ffa0215a4cbe9eeac7e2c9bebf86141052c
Matias Lappalainen <32136670+MatiasLappalainen@users.noreply.github.com>
2021-11-07 20:52:44 +0200

    Add postgres quories to posts route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,34 +1,27 @@
 var express = require('express');
+const { createClient } = require('../db/client');
 var router = express.Router();
 
-const posts = [{
-  headerImg: "https://loremflickr.com/1080/400",
-  title: "Koodikerho blogpost",
-  body: `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac tortor non urna aliquet gravida. Sed dignissim, elit sed lacinia ullamcorper, leo diam aliquet erat, et sagittis odio odio ac enim. Mauris ipsum nisl, consequat quis felis at, lobortis semper sapien. Aliquam a faucibus mi. Sed vehicula nunc quis tellus aliquam pulvinar. Sed ultricies, nunc a luctus pretium, orci sapien fermentum justo, nec commodo mi purus in lectus. Cras venenatis aliquet cursus. Morbi vitae metus et ligula venenatis faucibus. Etiam vel ex vitae ex mollis sodales quis sit amet neque. Cras semper vitae nibh sed tincidunt. Nam finibus mattis nibh, nec semper sem semper nec.
-  
-  Nulla mattis tellus ut dignissim tristique. Curabitur et sapien gravida, eleifend diam et, dignissim urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at consectetur sem, in pretium erat. Aenean nisi sem, dictum ac lacinia ornare, elementum quis sem. Maecenas ut finibus purus. Duis tincidunt ut diam at congue. Donec faucibus sapien enim, in scelerisque quam vestibulum a. Praesent tempus eget neque eu scelerisque. Cras felis justo, bibendum a mi in, sodales fermentum massa.
-  
-  Pellentesque venenatis laoreet enim, eu accumsan felis condimentum eget. Pellentesque non suscipit metus. Aenean hendrerit lorem ac massa pellentesque ornare. Sed rutrum est non massa egestas, eget vulputate mi mollis. Phasellus quis porta diam. Nullam nec pellentesque metus, at bibendum leo. Nullam consectetur molestie tellus, viverra tincidunt enim luctus quis. Sed commodo lacus leo, nec varius arcu blandit non.
-  
-  Nulla lorem turpis, dictum ornare elit vitae, accumsan iaculis ipsum. Vivamus id ultricies tortor. Morbi vitae ex ipsum. Sed pretium, orci non interdum maximus, nunc purus gravida nisl, vitae faucibus arcu sem nec sem. Sed vulputate non ligula vitae luctus. Quisque at posuere velit, non interdum enim. Vestibulum fringilla lobortis rhoncus.
-  
-  Quisque tempus odio sed ligula sodales, ut tempor magna varius. Sed venenatis dignissim felis, at viverra lorem pretium quis. Etiam imperdiet ac risus nec consequat. Pellentesque pulvinar volutpat lacus, nec hendrerit sem iaculis eu. Aliquam lacinia consequat turpis, et consectetur enim blandit eu. Nam eget interdum neque. Duis elementum venenatis turpis, vel convallis turpis aliquet vitae. Nam efficitur, justo sit amet ornare mollis, est orci imperdiet ante, non sollicitudin augue mi vitae dolor. Proin at ex tincidunt, luctus lectus vel, tristique quam. Aenean lacinia diam quis justo imperdiet, ut cursus velit ultrices.`
-}];
+const client = createClient();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200).send({ type: "BlogPost", data: posts });
+router.get('/', async (req, res, next) => {
+  const r = await client.query("SELECT * FROM posts")
+  res.status(200).send({ type: "BlogPost", data: r.rows });
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   if (!req.body.post) {
     res.status(400).send(req.body);
     return;
   }
   posts.push(req.body.post);
 
-  res.status(200).send({ type: "BlogPost", data: posts });
+  const { headerImg, title, body } = req.body.post;
+
+  const t = "INSERT INTO posts(img, title, body) VALUES($1, $2, $3) RETURNING *"
+  const r = await client.query(t, [headerImg, title, body]);
+  res.status(200).send({ type: "BlogPost", data: r.rows });
 })
 
 module.exports = router;
